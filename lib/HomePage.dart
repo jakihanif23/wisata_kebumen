9b9f93e82700539b87b8296f99bd107fda3d858c
jakihanif23 <jakiajjh48@gmail.com>
2021-10-27 04:10:27 +0700

    First commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _stream = FirebaseFirestore.instance.collection('wisata').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: _stream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            children: snapshot.data!.docs.map((document){
+              return Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width/1.1,
+                  height: MediaQuery.of(context).size.height/9,
+                  child: Card(
+                    child: ListTile(
+                      title: Text(document['nama']),
+                      onTap: ()=>print(Text(document['nama'])),
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
