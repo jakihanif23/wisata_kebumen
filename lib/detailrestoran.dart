0c8aeb6d1e50f7c902c3ac0a9368286e44087dcb
jakihanif23 <jakiajjh48@gmail.com>
2021-11-11 20:56:01 +0700

    creating detailrestoran.dart

---
+++ lib/detailrestoran.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class DetailRestoran extends StatefulWidget {
+  const DetailRestoran({Key? key}) : super(key: key);
+
+  @override
+  _DetailRestoranState createState() => _DetailRestoranState();
+}
+
+class _DetailRestoranState extends State<DetailRestoran> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
